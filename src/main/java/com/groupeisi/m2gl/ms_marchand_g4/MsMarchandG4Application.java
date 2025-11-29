553b03279b96b355308f41c85dc47f98b9693f40
Maïmouna SARR <101868721+maichou12@users.noreply.github.com>
2025-11-29 22:23:16 +0000

    initial commit

---
+++ src/main/java/com/groupeisi/m2gl/ms_marchand_g4/MsMarchandG4Application.java
@@ -0,0 +1,13 @@
+package com.groupeisi.m2gl.ms_marchand_g4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMarchandG4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMarchandG4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/groupeisi/m2gl/ms_marchand_g4/MsMarchandG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.m2gl.ms_marchand_g4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMarchandG4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
